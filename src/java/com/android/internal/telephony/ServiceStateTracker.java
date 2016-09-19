09d44287657afdb2420f517a9f0ec9aef8ceaef8
Arne Coucheron <arco68@gmail.com>
2016-09-19 06:53:52 +0200

    Telephony: Add option for using regular poll state for airplane mode
    The modemTriggeredPollState breaks airplane mode on some devices.
    Although the UI shows that airplane mode is activated, the fact is that
    the radio remains active and in service. By using regular pollState()
    here, service state in sim status shows correctly radio off.
    
    Enable with setting prop:
    ro.telephony.ril.config=legacypollstate
    
    Test: Insert SIM card. Put device into airplane mode. Go into
    SIM Status and verify that it says "Radio off".
    
    Change-Id: If2ab5b088a13b1a159e01f82fbea1f58a77b64cd
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/java/com/android/internal/telephony/ServiceStateTracker.java
+++ src/java/com/android/internal/telephony/ServiceStateTracker.java
@@ -1211,8 +1211,14 @@ public class ServiceStateTracker extends Handler {
                 }
                 // This will do nothing in the 'radio not available' case
                 setPowerStateToDesired();
-                // These events are modem triggered, so pollState() needs to be forced
-                modemTriggeredPollState();
+                if (needsLegacyPollState()) {
+                    // Some older radio blobs need this to put device
+                    // properly into airplane mode.
+                    pollState();
+                } else {
+                    // These events are modem triggered, so pollState() needs to be forced
+                    modemTriggeredPollState();
+                }
                 break;
 
             case EVENT_NETWORK_STATE_CHANGED:
@@ -5641,5 +5647,12 @@ public class ServiceStateTracker extends Handler {
         }
 
         return idSet;
+    }
+
+    private boolean needsLegacyPollState() {
+        if (mCi instanceof RIL) {
+            return ((RIL) mCi).needsOldRilFeature("legacypollstate");
+        }
+        return false;
     }
 }
