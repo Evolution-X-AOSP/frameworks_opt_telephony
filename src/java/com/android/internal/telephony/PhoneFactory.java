bf179ab6ab9f05f2bc64f7278816d9e74bcf552f
jiachunxu <leo.jia@oneplus.net>
2016-12-22 10:33:42 +0800

    [OP5] add for community - community is not pleased
    Change-Id: I54dd9d0bffab8cecef92c3a4fbc36c9a8cef7b52
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- src/java/com/android/internal/telephony/PhoneFactory.java
+++ src/java/com/android/internal/telephony/PhoneFactory.java
@@ -647,4 +647,11 @@ public class PhoneFactory {
         pw.flush();
         pw.decreaseIndent();
     }
+    //VENDOR_EDIT
+    /**@hide*/
+    public static PhoneSwitcher oemGetPhoneSwitcher()
+    {
+        return sPhoneSwitcher;
+    }
+    //end
 }
