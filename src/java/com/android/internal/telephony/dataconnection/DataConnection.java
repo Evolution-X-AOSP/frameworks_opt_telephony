354d11351e9d84bed3c83f744a67c53c5541c264
Joey Huab <joey@evolution-x.org>
2020-03-03 08:13:52 +0000

    Revert "FR57912: Fix to block data calls while retry pattern is enabled"
    This reverts commit ae9dc6e6dce3f748e4434444889000dfca08fc01.

--- src/java/com/android/internal/telephony/dataconnection/DataConnection.java
+++ src/java/com/android/internal/telephony/dataconnection/DataConnection.java
@@ -1801,11 +1801,6 @@ public class DataConnection extends StateMachine {
                     return HANDLED;
                 case EVENT_CONNECT:
                     if (DBG) log("DcInactiveState: mag.what=EVENT_CONNECT");
-                    if (isPdpRejectConfigEnabled() && !isDataCallConnectAllowed()) {
-                        if (DBG) log("DcInactiveState: skip EVENT_CONNECT");
-                        return HANDLED;
-                    }
-
                     ConnectionParams cp = (ConnectionParams) msg.obj;
 
                     if (!initConnection(cp)) {
@@ -1845,8 +1840,6 @@ public class DataConnection extends StateMachine {
                     }
                     if (mConnectionParams != null) {
                         if (initConnection(mConnectionParams)) {
-                            //In case of apn modify, get the latest apn to retry
-                            mApnSetting = mConnectionParams.mApnContext.getApnSetting();
                             connect(mConnectionParams);
                             transitionTo(mActivatingState);
                         } else {
@@ -3011,9 +3004,5 @@ public class DataConnection extends StateMachine {
     protected boolean isPdpRejectConfigEnabled() {
         return mPhone.getContext().getResources().getBoolean(
                 com.android.internal.R.bool.config_pdp_retry_for_29_33_55_enabled);
-    }
-
-    protected boolean isDataCallConnectAllowed() {
-        return true;
     }
 }
