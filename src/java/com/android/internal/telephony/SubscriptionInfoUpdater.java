3647d8bc288b041d3c0f358b6e123fb0a710c4bb
Pranav Vashi <neobuddy89@gmail.com>
2022-03-28 20:48:07 +0530

    Telephony: Add null check for operator
    * Pulled from CAF
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
+++ src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
@@ -517,7 +517,7 @@ public class SubscriptionInfoUpdater extends Handler {
                         sContext.getSystemService(Context.TELEPHONY_SERVICE);
                 String operator = tm.getSimOperatorNumeric(subId);
 
-                if (!TextUtils.isEmpty(operator)) {
+                if (operator != null && !TextUtils.isEmpty(operator)) {
                     if (subId == mSubscriptionController.getDefaultSubId()) {
                         MccTable.updateMccMncConfiguration(sContext, operator);
                     }
